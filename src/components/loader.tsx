40ccf94e618fecf8d383cb4c8b1038dd550ead04
Umair Khan <110655403+Umairmaseed@users.noreply.github.com>
2024-10-15 12:36:18 +0500

    loader created

---
+++ src/components/loader.tsx
@@ -0,0 +1,25 @@
+import { Box, Spinner, Image } from '@chakra-ui/react'
+
+const Loader = () => {
+  return (
+    <Box
+      position="fixed"
+      top="0"
+      left="0"
+      width="100vw"
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      bg="rgba(255, 255, 255, 0.4)"
+      backdropFilter="blur(2px)"
+      zIndex="1000"
+    >
+      <Box textAlign="center">
+        <Spinner size="xl" color="teal.500" />
+      </Box>
+    </Box>
+  )
+}
+
+export default Loader
