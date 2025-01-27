1fc766bea6633e59d1e1c5113b9cd71b56540ebf
Umair Khan <110655403+Umairmaseed@users.noreply.github.com>
2025-01-27 12:20:00 +0500

    services and api defined for contracts

--- src/services/contract.ts
+++ src/services/contract.ts
@@ -1,13 +1,44 @@
 import { api } from './api'
 
 export const ContractService = {
+  
   CreateContract: async (formData: CreateContract) => {
     return api
       .post('createcontract', formData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => res.data)
+  },
+  getUserContracts: async () => {
+    return api.get('getusercontracts').then((res) => res.data)
+  },
+  AddParticipants: async (formData: AddParticipants) => {
+    return api
+      .post('addparticipantrequest', formData, {
         headers: {
             'Content-Type': 'application/json',
         },
       })
       .then((res) => res.data)
-  }
+  },
+  AcceptInvite: async (token : string) => {
+    return api
+      .post(`addparticipants?token=${encodeURIComponent(token)}`, {}, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => res.data)
+  },
+  CancelContract: async ( formData: CancelContract) => {
+    return api
+      .post('canceldocument', formData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => res.data)
+  },
 }
