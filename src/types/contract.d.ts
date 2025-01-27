85c95f63b33b7eff2bcf7477dbde8e354cc6598b
Umair Khan <110655403+Umairmaseed@users.noreply.github.com>
2025-01-27 12:19:23 +0500

    contracts types defined

--- src/types/contract.d.ts
+++ src/types/contract.d.ts
@@ -1,4 +1,31 @@
 interface CreateContract {
-    name: string
-    signatureDate: string
+  name: string
+  signatureDate: string
+}
+
+interface AutoExecutableContract {
+  '@assetType': string
+  '@key': string
+  '@lastTouchBy': string
+  '@lastTx': string
+  '@lastUpdated': string
+  name: string
+  owner: UserWithKey
+  signatureDate: string
+  participants?: UserWithKey[]
+}
+
+interface ContractKey {
+    '@assetType': string
+    '@key': string
+    }
+
+interface AddParticipants {
+  autoExecutableContract: ContractKey
+  participants: UserKey[]
+}
+interface CancelContract {
+  clause: ContractKey
+  forceCancellation?: boolean
+  requestedCancellation?: boolean
 }
