f541d6c9939d0fc784347388f87cd9af7f79b93a
Umair Khan <110655403+Umairmaseed@users.noreply.github.com>
2025-01-27 12:22:43 +0500

    api integrated and listing of contract into dashboad

--- src/pages/ContractDashboard.tsx
+++ src/pages/ContractDashboard.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react'
 import {
   Box,
   Button,
@@ -17,27 +17,137 @@ import {
   Image,
   VStack,
   Heading,
-} from '@chakra-ui/react';
-import noDataImage from '../assets/Contract/no_data.jpg'; 
-import CreateContractForm from '../components/createContractForm';
+  useToast,
+} from '@chakra-ui/react'
+import noDataImage from '../assets/Contract/no_data.jpg'
+import CreateContractForm from '../components/createContractForm'
+import { ContractService } from '../services/contract'
+import { useAuth } from '../context/Authcontext'
+import { useQuery , useQueryClient} from '@tanstack/react-query'
+import { format } from 'date-fns';
+import AddParticipantForm from '../components/inviteParticipants'
 
 const Contract = () => {
-    const [isFormOpen, setIsFormOpen] = useState(false);
+  const [isFormOpen, setIsFormOpen] = useState(false)
+  const toast = useToast()
+  const { setLoading } = useAuth()
+  const [userCreatedContracts, setUserCreatedContracts] = useState<
+    AutoExecutableContract[]
+  >([])
+  const [userParticipatedContracts, setUserParticipatedContracts] = useState<
+    AutoExecutableContract[]
+  >([])
+  const [isParticipantFormOpen, setIsParticipantFormOpen] = useState(false);
 
-    const handleCreateContract = (data: { name: string; signatureDate: string }) => {
-      console.log('Contract Data:', data);
-      // Call API service here
-    };
-    
-  const contracts = []; // Example: Use state or props to manage contract data
+  const handleAddParticipants = async (newParticipants : UserKey[], contractId :string) => {
+    setLoading(true)
+   const req = {
+        "participants": newParticipants,
+        "autoExecutableContract": {
+            "@assetType": "autoExecutableContract",
+            "@key": contractId,
+        }
+
+    }
+
+    const response = await ContractService.AddParticipants(req)
+
+    if (response) {
+      toast({
+        title: 'Invite sent',
+        description: 'Invite sent successfully',
+        status: 'success',
+        duration: 3000,
+        isClosable: true,
+      })
+    } else {
+      toast({
+        title: 'Error',
+        description: 'An error occurred while sending the invite',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
+    }
+    setLoading(false)
+  };
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['UserContracts'],
+    queryFn: () => fetchUserContracts(),
+  })
+
+  const queryClient = useQueryClient()
+
+  const fetchUserContracts = async () => {
+    const response = await ContractService.getUserContracts()
+    return response
+  }
+
+  useEffect(() => {
+    setLoading(isLoading)
+
+    if (error) {
+      toast({
+        title: 'Error',
+        description: 'An error occurred while fetching the contracts',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
+    }
+
+    if (data && data.userCreatedContracts) {
+      setUserCreatedContracts(data.userCreatedContracts)
+    }
+    if (data && data.participantContract) {
+      setUserParticipatedContracts(data.participantContract)
+    }
+  }, [isLoading, error, data,toast])
+
+  const handleCreateContract = async (data: {
+    name: string
+    signatureDate: string
+  }) => {
+    setLoading(true)
+    const response = await ContractService.CreateContract(data)
+    queryClient.invalidateQueries({ queryKey: ['UserContracts'] })
+    if (response) {
+      toast({
+        title: 'Contract Created',
+        description: 'Contract created successfully',
+        status: 'success',
+        duration: 3000,
+        isClosable: true,
+      })
+    } else {
+      toast({
+        title: 'Error',
+        description: 'An error occurred while creating the contract',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
+    }
+  }
 
   return (
     <Box mx={20}>
-      <Box display="flex" justifyContent="space-between" alignItems="center" my={14}>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        my={14}
+      >
         <Heading size="xl" fontWeight="bold" color={'gray.700'}>
-         Dashboard
+          Dashboard
         </Heading>
-        <Button variant={'outline'} size={'sm'} colorScheme="blue" onClick={() => setIsFormOpen(true)}>
+        <Button
+          variant={'outline'}
+          size={'sm'}
+          colorScheme="blue"
+          onClick={() => setIsFormOpen(true)}
+        >
           Create Contract
         </Button>
       </Box>
@@ -45,6 +155,7 @@ const Contract = () => {
       <Tabs variant="enclosed">
         <TabList>
           <Tab>All Contracts</Tab>
+          <Tab>Participating</Tab>
           <Tab>In Progress</Tab>
           <Tab>Completed</Tab>
           <Tab>Cancelled</Tab>
@@ -52,72 +163,158 @@ const Contract = () => {
 
         <TabPanels>
           <TabPanel>
-            {contracts.length === 0 ? (
+            {userCreatedContracts.length === 0 ? (
               <VStack spacing={8} mt={14}>
                 <Image src={noDataImage} alt="No Contracts" boxSize="200px" />
                 <Text>No contracts found.</Text>
-                <Button colorScheme="blue" size={'sm'} onClick={() => setIsFormOpen(true)}>
+                <Button
+                  colorScheme="blue"
+                  size={'sm'}
+                  onClick={() => setIsFormOpen(true)}
+                >
                   Create Contract
                 </Button>
               </VStack>
             ) : (
-              <Table variant="simple">
-                <Thead>
-                  <Tr>
-                    <Th>ID</Th>
-                    <Th>Name</Th>
-                    <Th>Status</Th>
-                    <Th>Actions</Th>
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  {/* Example: Replace with dynamic rows */}
-                  <Tr>
-                    <Td>1</Td>
-                    <Td>Sample Contract</Td>
-                    <Td>In Progress</Td>
-                    <Td>
-                      <Button size="sm" colorScheme="teal" mr={2}>
-                        View
-                      </Button>
-                      <Button size="sm" colorScheme="red">
-                        Cancel
-                      </Button>
-                    </Td>
-                  </Tr>
-                </Tbody>
-              </Table>
+              <>
+                <Table variant="simple">
+                  <Thead bg={'gray.100'}>
+                    <Tr>
+                      <Th>Name</Th>
+                      <Th>Signature Date</Th>
+                      <Th>Owner</Th>
+                      <Th>Last Updated</Th>
+                      <Th>Participants</Th>
+                      <Th textAlign={'center'}>Actions</Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {userCreatedContracts.map((contract, index) => (
+                      <Tr key={index}>
+                        <Td>{contract.name}</Td>
+                        <Td>
+                        {contract.signatureDate
+                            ? format(new Date(contract.signatureDate), 'dd MMM, yyyy')
+                            : '-'}
+                        </Td>
+                        <Td>{contract.owner.name}</Td>
+                        <Td>
+                        {contract['@lastUpdated']
+                            ? format(new Date(contract['@lastUpdated']), 'dd MMM, yyyy')
+                            : '-'}
+                        </Td>
+                        <Td>
+                          {contract.participants &&
+                          contract.participants.length > 0
+                            ? contract.participants
+                                .map((participant, index) => participant.name)
+                                .join(', ')
+                            : '-'}
+                        </Td>
+                        <Td>
+                          <Button size="sm" colorScheme="teal" mr={2}>
+                            Add Clause
+                          </Button>
+                          <Button size="sm" colorScheme="teal" mr={2}         onClick={() => setIsParticipantFormOpen(true)}
+                          >
+                            Add Participants
+                          </Button>
+                          <Button size="sm" colorScheme="red">
+                            Cancel
+                          </Button>
+                          <AddParticipantForm
+                                isOpen={isParticipantFormOpen}
+                                onClose={() => setIsParticipantFormOpen(false)}
+                                onAddParticipant={handleAddParticipants}
+                                contractId={contract['@key']}
+                            />
+                        </Td>
+                      </Tr>
+                    ))}
+                  </Tbody>
+                </Table>
+              </>
             )}
           </TabPanel>
 
-          {/* Repeat similar structure for other tabs */}
           <TabPanel>
-          <VStack spacing={8} mt={14}>
+            {userParticipatedContracts.length === 0 ? (
+              <VStack spacing={8} mt={14}>
                 <Image src={noDataImage} alt="No Contracts" boxSize="200px" />
-                <Text>No contracts in progress.</Text>
+                <Text>No contracts found.</Text>
               </VStack>
+            ) : (
+              <>
+                <Table variant="simple">
+                  <Thead bg={'gray.100'}>
+                    <Tr>
+                      <Th>Name</Th>
+                      <Th>Signature Date</Th>
+                      <Th>Owner</Th>
+                      <Th>Last Updated</Th>
+                      <Th>Participants</Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {userParticipatedContracts.map((contract, index) => (
+                      <Tr key={index}>
+                        <Td>{contract.name}</Td>
+                        <Td>
+                        {contract.signatureDate
+                            ? format(new Date(contract.signatureDate), 'dd MMM, yyyy')
+                            : '-'}
+                        </Td>
+                        <Td>{contract.owner.name}</Td>
+                        <Td>
+                        {contract['@lastUpdated']
+                            ? format(new Date(contract['@lastUpdated']), 'dd MMM, yyyy')
+                            : '-'}
+                        </Td>
+                        <Td>
+                          {contract.participants &&
+                          contract.participants.length > 0
+                            ? contract.participants
+                                .map((participant, index) => participant.name)
+                                .join(', ')
+                            : '-'}
+                        </Td>
+                      </Tr>
+                    ))}
+                  </Tbody>
+                </Table>
+              </>
+            )}
           </TabPanel>
           <TabPanel>
-          <VStack spacing={8} mt={14}>
-                <Image src={noDataImage} alt="No Contracts" boxSize="200px" />
-                <Text>No contracts completed yet.</Text>
-              </VStack>
+            <VStack spacing={8} mt={14}>
+              <Image src={noDataImage} alt="No Contracts" boxSize="200px" />
+              <Text>No contracts in progress.</Text>
+            </VStack>
           </TabPanel>
           <TabPanel>
-          <VStack spacing={8} mt={14}>
-                <Image src={noDataImage} alt="No Contracts" boxSize="200px" />
-                <Text>No contracts canceled.</Text>
-              </VStack>
+            <VStack spacing={8} mt={14}>
+              <Image src={noDataImage} alt="No Contracts" boxSize="200px" />
+              <Text>No contracts completed yet.</Text>
+            </VStack>
+          </TabPanel>
+          <TabPanel>
+            <VStack spacing={8} mt={14}>
+              <Image src={noDataImage} alt="No Contracts" boxSize="200px" />
+              <Text>No contracts canceled.</Text>
+            </VStack>
           </TabPanel>
         </TabPanels>
       </Tabs>
       <>
-      {isFormOpen && (
-        <CreateContractForm onClose={() => setIsFormOpen(false)} onSubmit={handleCreateContract} />
-      )}
-    </>
+        {isFormOpen && (
+          <CreateContractForm
+            onClose={() => setIsFormOpen(false)}
+            onSubmit={handleCreateContract}
+          />
+        )}
+      </>
     </Box>
-  );
-};
+  )
+}
 
-export default Contract;
+export default Contract
