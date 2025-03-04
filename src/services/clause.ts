9131c2d815356bb149709efd77486bbf0d06af96
Umair Khan <110655403+Umairmaseed@users.noreply.github.com>
2025-03-04 19:49:05 +0500

    endpoints added to add inputs to cdi

--- src/services/clause.ts
+++ src/services/clause.ts
@@ -10,4 +10,27 @@ export const ClauseService = {
       })
       .then((res) => res)
   },
+  GetClause: async (clasueKey: string) => {
+    return api
+      .get(`getclause?clauseKey=${encodeURIComponent(clasueKey)}`)
+      .then((res) => res.data)
+  },
+  AddEvaluteDate: async (formData: any) => {
+    return api
+      .post('addevaluatedate', formData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => res)
+    },
+  AddReferenceDate: async (formData: any) => {
+    return api
+      .post('addreferencedate', formData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => res)
+  },
 }
