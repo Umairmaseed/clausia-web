7b0e79f5651ca4dceed3a57752c73a792614e7fd
Umair Khan <110655403+Umairmaseed@users.noreply.github.com>
2025-03-24 19:46:51 +0500

    api added to add inputs to get deduction

--- src/services/clause.ts
+++ src/services/clause.ts
@@ -43,13 +43,19 @@ export const ClauseService = {
       .then((res) => res)
   },
   AddInputsToMakePayment: async (formData: any) => {
-    for (let pair of formData.entries()) {
-      console.log(`${pair[0]}: ${pair[1]} (${typeof pair[1]})`)
-    }
     return api
       .post('addinputstomakepayment', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then((res) => res)
+  },
+  AddInputsToGetDeduction: async (formData: any) => {
+    return api
+      .post('addinputstocheckfine', JSON.stringify(formData), {
+        headers: {
+          'Content-Type': 'application/json',
         },
       })
       .then((res) => res)
